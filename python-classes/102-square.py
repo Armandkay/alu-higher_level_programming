92d9487cafabf94716b4ceeed6be9aa547a4a65b
Armandkay <a.kayiranga1@alustudent.com>
2023-11-11 03:34:29 -0800

    102-square.py

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""creates class Square with
-private instance attribute size and public instance method"""
+Square = __import__('102-square').Square
 
+s_5 = Square(5)
+s_6 = Square(6)
 
-class Square:
-    """defines class with instantiated and validated private instance attribute
-and public instance method."""
-
-    def __init__(self, size=0):
-        self.__size = size
-
-    @property
-    def size(self):
-        return(self.__size)
-
-    @size.setter
-    def size(self, value):
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """calculates and returns current square area"""
-        return(self.__size * self.__size)
+if s_5 < s_6:
+    print("Square 5 < Square 6")
+if s_5 <= s_6:
+    print("Square 5 <= Square 6")
+if s_5 == s_6:
+    print("Square 5 == Square 6")
+if s_5 != s_6:
+    print("Square 5 != Square 6")
+if s_5 > s_6:
+    print("Square 5 > Square 6")
+if s_5 >= s_6:
+    print("S
