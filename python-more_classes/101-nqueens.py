008b82b67c3d4c521344385e0c3301a55ea445a7
Armandkay <a.kayiranga1@alustudent.com>
2023-11-11 11:25:51 -0800

    101-nqueens.py

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,15 +1,47 @@
 #!/usr/bin/python3
-"""defines program to solve the N queens problem"""
-
-
 import sys
-if len(sys.args) is not 2:
-    print("Usage: nqueens N")
-    exit(1)
-N = sys.args[1]
-if type(N) is not int:
-    print("N must be a number")
-    exit(1)
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
+
+def print_solution(solution):
+    print(solution)
+
+def is_safe(board, row, col, n):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+def solve_nqueens(board, row, n):
+    if row == n:
+        print_solution([[i, board[i]] for i in range(n)])
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row] = col
+            solve_nqueens(board, row + 1, n)
+
+def nqueens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    solve_nqueens(board, 0, n)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} N".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
