2838bd04e4e4bb49eb6b1289ecae38422bcbcd00
Armandkay <a.kayiranga1@alustudent.com>
2023-11-29 01:14:06 -0800

    1-create_user.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,13 @@
--- A script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
--- Query to list all privileges 
+-- Grant privileges to user_0d_1
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant privileges to user_0d_2
+CREATE USER 'user_0d_2'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'user_0d_2'@'localhost';
+
+-- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
