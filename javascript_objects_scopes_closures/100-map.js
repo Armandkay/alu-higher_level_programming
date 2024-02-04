c355fb30921b359fcac0b47dd4c1fda8d782d340
Armandkay <a.kayiranga1@alustudent.com>
2024-02-04 02:42:32 -0800

    100-map.js

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-// Import the list from the file
-const { list } = require('./100-data.js');
 
-// Compute a new array using map
+const { list } = require('./100-data');
+
 const newList = list.map((value, index) => value * index);
 
-// Print both the initial array and the new array
-console.log('Initial list:', list);
-console.log('New list:', newList);
+console.log(list);
+console.log(newList);
