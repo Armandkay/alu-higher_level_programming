ae9803545f91c8a27c5c109aef7cf0b032d6faf5
Armandkay <a.kayiranga1@alustudent.com>
2024-02-04 02:49:00 -0800

    101-sorted.js

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -6,11 +6,11 @@ const { dict } = require('./101-data');
 const sortedDict = {};
 
 for (const userId in dict) {
-    const occurrences = dict[userId];
-    if (sortedDict[occurrences] === undefined) {
-        sortedDict[occurrences] = [];
-    }
-    sortedDict[occurrences].push(userId);
+  const occurrences = dict[userId];
+  if (sortedDict[occurrences] === undefined) {
+    sortedDict[occurrences] = [];
+  }
+  sortedDict[occurrences].push(userId);
 }
 
 console.log(sortedDict);
