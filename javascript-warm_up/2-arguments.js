a7ec0c80bfbac72b6c76f571579a8af1d65f5f82
Armandkay <a.kayiranga1@alustudent.com>
2024-02-04 01:25:10 -0800

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (!args.length) {
+const myArgs = process.argv.slice(2);
+if (myArgs.length === 0) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (myArgs.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
+}
