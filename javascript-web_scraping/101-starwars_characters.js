88b043a3edef44001fa49aa29beb064a3ebbdc10
Armandkay <a.kayiranga1@alustudent.com>
2024-02-11 04:12:56 -0800

    101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,41 +1,29 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie
 const request = require('request');
-
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a movie ID');
-  process.exit(1);
-}
-
-const url = `https://swapi.dev/api/films/${movieId}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+const myArgs = process.argv.slice(2);
+const URLstring = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+function characterFunc (character, i = 0, stop) {
+  if (i === stop) {
+    return;
   }
-
-  const film = JSON.parse(body);
-
-  if (film.title === 'Not Found') {
-    console.error('Movie not found');
-    process.exit(1);
-  }
-
-  const charactersUrls = film.characters;
-
-  charactersUrls.forEach((charactersUrl, index) => {
-    request(charactersUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        process.exit(1);
-      }
-
-      const character = JSON.parse(body);
-
-      console.log(`${index + 1}. ${character.name}`);
-    });
+  const URLcharacter = character[i];
+  request(URLcharacter, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+    }
   });
+  i++;
+  characterFunc(character, i, stop);
+}
+request(URLstring, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const character = JSON.parse(body).characters;
+    characterFunc(character, 0, character.length);
+  }
 });
+
